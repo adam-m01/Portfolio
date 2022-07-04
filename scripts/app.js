14004bc0f97e11d5a95ad8ba17a98c7253c59d54
Adam <adam.m01@hotmail.com>
2022-07-04 21:17:42 +0100

    UI Update 🚀

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   $(".main").scroll(function () {
     var scrollTop = $(this).scrollTop();
 
-    $(".scroll-down").css({
+    $(".scroll-down ").css({
       opacity: function () {
         var elementHeight = $(this).height();
         return 1 - scrollTop / elementHeight;
@@ -17,34 +17,18 @@ $(document).ready(function () {
   var scroll_pos = 0;
   $(".main").scroll(function () {
     scroll_pos = $(this).scrollTop();
-    if (scroll_pos < 600) {
+    if (scroll_pos < 500) {
       $("#homeCircle").css("background", "white");
       $("#projectsCircle").css("background", "none");
       $("#contactCircle").css("background", "none");
-    } else if (scroll_pos > 600 && scroll_pos < 1400) {
+    } else if (scroll_pos > 500 && scroll_pos < 1200) {
       $("#homeCircle").css("background", "none");
       $("#projectsCircle").css("background", "white");
       $("#contactCircle").css("background", "none");
-    } else if (scroll_pos > 1400) {
+    } else if (scroll_pos > 1200) {
       $("#homeCircle").css("background", "none");
       $("#projectsCircle").css("background", "none");
       $("#contactCircle").css("background", "white");
     }
   });
-
-  // Fade in elements
-  // $('.main').scroll(function() {
-  //   var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-  //   $(".fade").each(function() {
-  //     /* Check the location of each desired element */
-  //     var objectBottom = $(this).offset().top + $(this).outerHeight();
-      
-  //     /* If the element is completely within bounds of the window, fade it in */
-  //     if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-  //       if ($(this).css("opacity")==0) {$(this).fadeTo(200,1);}
-  //     } else { //object goes out of view (scrolling up)
-  //       if ($(this).css("opacity")==1) {$(this).fadeTo(200,0);}
-  //     }
-  //   });
-  // }).scroll(); //invoke scroll-handler on page-load
 });
